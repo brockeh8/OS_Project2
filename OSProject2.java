88b9cbe52754a2c6c956c17d6fceae283ff37624
ggiacobbe2 <ggiacobbe13@gmail.com>
2025-04-28 17:33:15 -0400

    Update OSProject2.java
    fixing syntax

--- OSProject2.java
+++ OSProject2.java
@@ -1,6 +1,6 @@
 public class OSProject2 {
   public static void main(String[] args) throws Exception {
-    Process.Simulator.run("processes.txt");
+    ProcessSimulator.run("processes.txt");
     DiningManager.run(5,3);
   }
 }
