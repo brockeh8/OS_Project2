346ed82900b6315d395dce3fcc13969ef20f66da
brockeh8 <122937812+brockeh8@users.noreply.github.com>
2025-04-28 18:14:28 -0400

    Update OSProject2.java

--- OSProject2.java
+++ OSProject2.java
@@ -1,6 +1,6 @@
 public class OSProject2 {
   public static void main(String[] args) throws Exception {
     ProcessSimulator.run("processes.txt");
-    DiningManager.run(5,3);
+    DiningManager.run(5,2);
   }
 }
