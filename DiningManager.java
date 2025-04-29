2f1789b3bdfb03c4312787173897dda5c5db921b
ggiacobbe2 <ggiacobbe13@gmail.com>
2025-04-29 17:10:04 -0400

    Create DiningManager.java
    dining manager created

---
+++ DiningManager.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.Semaphore;
+
+public class DiningManager {
+  public static void run(NumPhilosophers, meals) throws InterruptedException {
+    Semaphore[] forks = new Semaphore[numPhilosophers];
+    for (int i = 0; i < numPhilosophers; i++) {
+      forks[i] = new Semaphore;
+    }
+
+    Thread[] threads = new Thread[numPhilosophers];
+    for (int i = 0; i < numPhilosophers; i++) {
+      threads[i] = new Thread(new Philosopher(i + 1, forks, meals);
+      threads[i].start();
+    }
+
+    for (Thread t : threads) t.join();
+
+    System.out.println("Dining Philosphers simulation complete");
+  }
+}
